b22f46af3d99b4bfe739183780dae571b5232289
Hr824 <bdv.pro@gmail.com>
2024-06-30 00:35:01 +0200

    Add ng-bootstrap and create navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/errors/page-not-found/page-not-found.component';
+import { ErrorOccurredComponent } from './pages/errors/error-occurred/error-occurred.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'errorOccurred',
+        component: ErrorOccurredComponent
+    },
+    {
+      path: '**',
+      component: PageNotFoundComponent
+    }
+];
---
+++ src/app/pages/errors/error-occurred/error-occurred.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-error-occurred',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './error-occurred.component.html',
+  styleUrl: './error-occurred.component.css'
+})
+export class ErrorOccurredComponent {
+
+}
