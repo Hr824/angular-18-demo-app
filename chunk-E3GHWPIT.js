6103d6aa71c5a26c31b694b1ea52ac3861747090
Hr824 <bdv.pro@gmail.com>
2024-09-03 13:05:35 +0200

    Auto-generated commit

---
+++ chunk-E3GHWPIT.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-J3T55C4R.js";import"./chunk-NU7VILO4.js";import"./chunk-LS5FHDU7.js";import"./chunk-2NFLSA4Y.js";var p=[{path:"forkjoin",loadComponent:()=>import("./chunk-GV4N54Y5.js").then(n=>n.ForkjoinComponent)},{path:"**",component:o}];export{p as rxjsRoutes};
